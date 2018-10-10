c34f8247efbf58b0accf766050245d17639e2dff
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2018-10-10 14:34:04 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,116 @@
+var service_data = [
+	{	
+		ikona: 'paint-brush',
+		title: 'WEB DESIGN',
+		tekstas: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum.',
+	},
+	{	
+		ikona: 'mobile',
+		title: 'MOBILE APPS',
+		tekstas: 'Ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo.',
+	},
+	{	
+		ikona: 'code',
+		title: 'WEB DEVELOPMENT',
+		tekstas: 'Maecenas efficitur massa nec quam imperdiet, a luctus magna egestas. Praesent hendrerit metus vel justo ornare, ullamcorper molestie lorem ultrices.',
+	},
+];
+
+
+var portfolio_gallery = [
+{
+	name: 'Real Work',
+	tag: 'Ados',
+	picture: 'dino',
+	url: 'coming/index.html'
+},
+{
+	name: 'Photoshop Work',
+	tag: 'app',
+	picture: 'katinas',
+	url: '404 page/index.html'
+},
+{
+	name: 'Illustration Work',
+	tag: 'app',
+	picture: 'obuolys',
+	url: '404 page/index.html'
+},
+{
+	name: 'Real Work',
+	tag: 'Ados',
+	picture: 'cog',
+	url: 'checklist/index.html'
+},
+{
+	name: 'Photoshop Work',
+	tag: 'website',
+	picture: 'lape',
+	url: '404 page/index.html'
+},
+{
+	name: 'Website Work',
+	tag: 'illustration',
+	picture: 'laivelis',
+	url: '404 page/index.html'
+},
+{
+	name: 'Apps Work',
+	tag: 'illustration',
+	picture: 'dramblys',
+	url: '404 page/index.html'
+},
+
+]
+
+var resume_items = [
+{
+	type: 'Work',
+	place: 'Apple',
+	date: 'Oct 2015 - June 2016',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
+},
+{
+	type: 'Work',
+	place: 'Google',
+	date: 'Oct 2016 - June 2017',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
+},
+{
+	type: 'Education',
+	place: 'University',
+	date: 'Oct 2010 - June 2013',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
+},
+{
+	type: 'Education',
+	place: 'Desing and art',
+	date: 'Oct 2013 - June 2015',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
+},
+]
+
+var blog_items = [
+{
+	picture: 'orange',
+	title: 'New Blog For Web Designer',
+	date: '25th Oct 2017',
+	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
+},
+{
+	picture: 'apples',
+	title: 'Latest Mobile Apps',
+	date: '29th Oct 2017',
+	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
+},
+{
+	picture: 'colors',
+	title: 'You Need Website For Your Business',
+	date: '25th Nov 2017',
+	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
+},
+
+]
+
+
+
