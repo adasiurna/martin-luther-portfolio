92f300105c8fa3dba313e6a83fb6700397222dcd
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2021-04-12 01:46:04 +0300

    updated portfolio section

--- js/data.js
+++ js/data.js
@@ -1,114 +1,107 @@
-var service_data = [
-	{	
-		ikona: 'paint-brush',
-		title: 'WEB DESIGN',
-		tekstas: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum.',
-	},
-	{	
-		ikona: 'mobile',
-		title: 'MOBILE APPS',
-		tekstas: 'Ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo.',
-	},
-	{	
-		ikona: 'code',
-		title: 'WEB DEVELOPMENT',
-		tekstas: 'Maecenas efficitur massa nec quam imperdiet, a luctus magna egestas. Praesent hendrerit metus vel justo ornare, ullamcorper molestie lorem ultrices.',
-	},
-];
-
-
 var portfolio_gallery = [
-{
-	name: 'Real Work',
-	tag: 'Ados',
-	picture: 'dino',
-	url: 'coming/index.html'
-},
-{
-	name: 'Photoshop Work',
-	tag: 'app',
-	picture: 'katinas',
-	url: '404 page/index.html'
-},
-{
-	name: 'Illustration Work',
-	tag: 'app',
-	picture: 'obuolys',
-	url: '404 page/index.html'
-},
-{
-	name: 'Real Work',
-	tag: 'Ados',
-	picture: 'cog',
-	url: 'checklist/index.html'
-},
-{
-	name: 'Photoshop Work',
-	tag: 'website',
-	picture: 'lape',
-	url: '404 page/index.html'
-},
-{
-	name: 'Website Work',
-	tag: 'illustration',
-	picture: 'laivelis',
-	url: '404 page/index.html'
-},
-{
-	name: 'Apps Work',
-	tag: 'illustration',
-	picture: 'dramblys',
-	url: '404 page/index.html'
-},
+  {
+    name: 'Random quotes project for FCC',
+    tag: 'React',
+    picture: 'quotes_img',
+    url: 'https://codepen.io/adasiurna/full/eYdyLVp'
+  },
+  {
+    name: 'Interval sport timer build with React',
+    tag: 'React',
+    picture: 'timer_img',
+    url: 'https://adasiurna.github.io/timer-reactjs'
+  },
+  {
+    name: 'Ugly Portfolio',
+    tag: 'JavaScript',
+    picture: 'portfolio_img',
+    url: 'https://codepen.io/adasiurna/full/ZqJeMO/'
+  },
+  {
+    name: 'Station locator',
+    tag: 'JavaScript',
+    picture: 'maps_img',
+    url: 'https://codepen.io/adasiurna/full/NWGqVKW'
+  },
+  {
+    name: 'Matrox landing page',
+    tag: 'HTML',
+    picture: 'matrox_img',
+    url: 'https://adasiurna.github.io/martin-luther-portfolio/coming/coming.html'
+  },
+  {
+    name: 'Simple survey layout',
+    tag: 'HTML',
+    picture: 'survey_img',
+    url: 'https://codepen.io/adasiurna/full/wQwXEK'
+  },
+  {
+    name: 'Documentation layout',
+    tag: 'HTML',
+    picture: 'documentation_img',
+    url: 'https://codepen.io/adasiurna/full/VqxoNG'
+  },
+  {
+    name: '404 error page',
+    tag: 'HTML',
+    picture: 'katinas',
+    url: '404 page/index.html'
+  }
 
 ]
 
 var resume_items = [
-{
-	type: 'Work',
-	place: 'Apple',
-	date: 'Oct 2015 - June 2016',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
-},
-{
-	type: 'Work',
-	place: 'Google',
-	date: 'Oct 2016 - June 2017',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
-},
-{
-	type: 'Education',
-	place: 'University',
-	date: 'Oct 2010 - June 2013',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
-},
-{
-	type: 'Education',
-	place: 'Desing and art',
-	date: 'Oct 2013 - June 2015',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque.'
-},
+  {
+    type: 'Work',
+    place: 'UAB "WorldOne" - Survey Programmer',
+    date: 'Aug 2019 - Onwards',
+    description: 'Programming online surveys using ConfirmIt platform and WEB technologies (JavaScript, CSS and HTML); Clients consulting for improving the accuracy and efficiency of their Questionnaire by suggesting modifications to online survey logic and layout; Managing clients specific requests; Managing internal communication with other team members/departments.'
+  },
+  {
+    type: 'Work',
+    place: 'UAB Intrado - Web Developer',
+    date: 'Feb 2019 - Aug 2019',
+    description: 'Development and rebuilding of existing websites, creating responsive front end themes for Drupal 8. Writing, debugging and maintaining web applications using a variety of languages and platforms such as Drupal 8, HTML, PHP, Twig, CSS3, JavaScript.'
+  },
+  {
+    type: 'Education',
+    place: 'Baltic Talents Academy',
+    date: 'Aug 2018 - Feb 2019',
+    description: 'Web programming. Front-End development using HTML, CSS, JavaScript, React. Back-End development using PHP, Node.js, Express.js. Other technologies used: Git, MySQL, MongoDB, npm, Postman and more.'
+  },
+  {
+    type: 'Education',
+    place: 'Vilnius University',
+    date: 'Sep 2004 - June 2011',
+    description: 'Master of Educational Psychology'
+  },
+  {
+    type: 'Education',
+    place: 'Vilnius University',
+    date: 'Sep 2003 - June 2004',
+    description: 'Bachelor of Informatics'
+  }
 ]
 
 var blog_items = [
-{
-	picture: 'orange',
-	title: 'New Blog For Web Designer',
-	date: '25th Oct 2017',
-	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
-},
-{
-	picture: 'apples',
-	title: 'Latest Mobile Apps',
-	date: '29th Oct 2017',
-	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
-},
-{
-	picture: 'colors',
-	title: 'You Need Website For Your Business',
-	date: '25th Nov 2017',
-	description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
-},
+  {
+    picture: 'orange',
+    title: 'New Blog For Web Designer',
+    date: '25th Oct 2017',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
+  },
+  {
+    picture: 'apples',
+    title: 'Latest Mobile Apps',
+    date: '29th Oct 2017',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
+  },
+  {
+    picture: 'colors',
+    title: 'You Need Website For Your Business',
+    date: '25th Nov 2017',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy.'
+  },
 
 ]
 
